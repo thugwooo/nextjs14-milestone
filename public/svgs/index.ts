5bb6a5e27dba4d25aeec50c5dc9cccb97c99cd03
LeeSeokWoo <indians1720@gmail.com>
2024-04-22 18:16:57 +0900

    기초 공사

---
+++ public/svgs/index.ts
@@ -0,0 +1,31 @@
+// import IconInfo from './info.svg';
+// import IconComment from './comment.svg';
+// import IconGithub from './github.svg';
+// import IconEyeTemplate from './eye-template.svg';
+// import IconEyeBall from './eye-ball.svg';
+// import IconEyeClosed from './eye-closed.svg';
+// import IconClose from './close.svg';
+// import IconEdit from './edit.svg';
+// import IconChevronRight from './chevron-right.svg';
+// import IconProviderFacebook from './provider-facebook.svg';
+// import IconProviderGithub from './provider-github.svg';
+// import IconProviderGoogle from './provider-google.svg';
+// import IconRSS from './rss.svg';
+// import IconSearch from './search.svg';
+
+// export {
+//   IconInfo,
+//   IconComment,
+//   IconGithub,
+//   IconEyeTemplate,
+//   IconEyeBall,
+//   IconEyeClosed,
+//   IconClose,
+//   IconEdit,
+//   IconChevronRight,
+//   IconProviderFacebook,
+//   IconProviderGithub,
+//   IconProviderGoogle,
+//   IconRSS,
+//   IconSearch,
+// };
